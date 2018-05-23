262d59c9db9cccebffc3c8b4ab98a2ae5b90a803
Syril <syrilster@gmail.com>
2018-05-23 18:54:41 +0530

    After adding dynamic port using an Environment property.

--- src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.springboot.microservices.currencyexchangeservice;
 
 import com.springboot.microservices.currencyexchangeservice.bean.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,13 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(1L, from, to, new BigDecimal(61));
+        ExchangeValue exchangeValue = new ExchangeValue(1L, from, to, new BigDecimal(61));
+        exchangeValue.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+        return exchangeValue;
     }
 }
