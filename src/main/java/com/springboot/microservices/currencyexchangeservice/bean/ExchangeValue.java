36eb8370422331feb6e46f5a611afcb8e19385be
Syril <syrilster@gmail.com>
2018-05-23 19:13:09 +0530

    After adding JPA support + insert script.

--- src/main/java/com/springboot/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -1,14 +1,21 @@
 package com.springboot.microservices.currencyexchangeservice.bean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Created by Syril on 23-05-2018.
  */
+@Entity
 public class ExchangeValue {
 
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private int port;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1000, 'USD', 'INR', 67, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1001, 'AUD', 'INR', 51, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(1002, 'EUR', 'INR', 82, 0);
