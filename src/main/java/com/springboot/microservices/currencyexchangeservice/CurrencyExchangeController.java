55677c64ae2a39847730af0c0558dc9a919fbf0e
Syril <syrilster@gmail.com>
2018-05-26 09:09:38 +0530

    After adding Slueth distributed tracing.

--- src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.springboot.microservices.currencyexchangeservice;
 
 import com.springboot.microservices.currencyexchangeservice.bean.ExchangeValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
  */
 @RestController
 public class CurrencyExchangeController {
+    Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private Environment environment;
@@ -24,7 +27,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
-
+        logger.info("Exchange Value {} " + exchangeValue);
         return exchangeValue;
     }
 }
