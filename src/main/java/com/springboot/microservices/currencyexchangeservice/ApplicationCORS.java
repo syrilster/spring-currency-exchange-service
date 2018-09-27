8826763537608375af682a1c72f20a84e4f19502
Syril <syrilster@gmail.com>
2018-09-27 11:26:25 +1000

    Added CORS at global level, method level and at the class level to allow requests from React JS application. Disable RabbitMQ config from the classpath. Enable Zipkin dependency on classpath.

---
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/ApplicationCORS.java
@@ -0,0 +1,18 @@
+package com.springboot.microservices.currencyexchangeservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationCORS implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
--- src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/springboot/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import java.math.BigDecimal;
 /**
  * Created by Syril on 23-05-2018.
  */
+
+//@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 public class CurrencyExchangeController {
     Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
@@ -23,6 +26,7 @@ public class CurrencyExchangeController {
     @Autowired
     private ExchangeValueRepository repository;
 
+    //@CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
